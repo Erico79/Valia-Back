866fab9c2fbf2cd5ef332cf9d0e9dc76c49e8369
Eric Sensus <eric@sensussystems.com>
2017-07-20 18:34:31 +0300

    begin validation

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Title} from "@angular/platform-browser";
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-registration',
@@ -7,11 +7,24 @@ import {Title} from "@angular/platform-browser";
   styleUrls: ['../../assets/css/material-kit.css']
 })
 export class RegistrationComponent implements OnInit {
+  private pass_match: boolean;
+  private form_valid: boolean;
 
-  constructor(private title: Title) {}
+  constructor(private title: Title) {
+    this.pass_match = false;
+    this.form_valid = false;
+  }
 
   ngOnInit(): void {
     this.title.setTitle('Register');
+  }
+
+  checkPassword(pass1: string, pass2: string) {
+    if (pass1 === pass2) {
+      this.pass_match = true;
+    } else {
+      this.pass_match = false;
+    }
   }
 
 }
