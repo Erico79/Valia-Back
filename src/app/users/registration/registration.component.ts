8462267c7c7c58f7a74af2a6bd5b2db879593e01
Eric Sensus <eric@sensussystems.com>
2017-07-26 13:52:49 +0300

    Start working on api

--- src/app/users/registration/registration.component.ts
+++ src/app/users/registration/registration.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Title} from '@angular/platform-browser';
 
-import {Seller} from './seller';
+import {Seller} from '../seller';
+import {UserService} from '../user.service';
 
 @Component({
   selector: 'app-registration',
@@ -9,19 +10,31 @@ import {Seller} from './seller';
   styleUrls: [
     '../../../assets/css/material-kit.css',
     './registration.component.css'
-  ]
+  ],
+  providers: [UserService]
 })
 export class RegistrationComponent implements OnInit {
   private submitted: boolean;
-  seller = new Seller('', '', '', '', '', '', '', false);
+  private api_errors: string[];
+  seller = new Seller('Eric', 'Murimi Njue', 'emurinyo@gmail.com', '0718513948', 'pass123', 'pass123', 'Lara Fashions', false);
 
-  constructor(private title: Title) {}
+  constructor(
+    private title: Title,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
     this.title.setTitle('Register');
   }
 
   onSubmit(): void {
-
+    this.userService.createSeller(this.seller)
+      .then(function (data) {
+        if (data.success) {
+          
+        } else {
+          // loop and show server side error messages
+        }
+      });
   }
 }
--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import {Seller} from './seller';
+
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class UserService {
-  private userUrl = '//localhost/valia-api/api/user';
+  private userUrl = '//localhost/valia-api/public/api/seller';
+  private headers = new Headers({'Content-Type': 'application/json'});
 
-  createSeller(): void {
+  constructor(private http: Http) {}
 
+  createSeller(merchant: Seller): Promise<any> {
+    return this.http.post(this.userUrl, JSON.stringify(merchant), {headers: this.headers})
+      .toPromise()
+      .then(response => response.json())
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+      console.error('An error occurred', error);
+      return Promise.reject(error.message || error);
   }
 }
